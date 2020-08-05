3e10fb0e6e095d2ca5151af300a3621a209645ee
Jane Wang <yutinglianwang@google.com>
2020-08-05 15:28:19 -0700

    added new GetInferenceResult function and clean up print function

--- src/multiple_edgetpu_demo.cc
+++ src/multiple_edgetpu_demo.cc
@@ -73,17 +73,26 @@ struct Container {
   int interpreter_inference_count = 0;
   GstElement *pipeline;
   std::unordered_map<int, std::string> labels;
+  bool visualize_inference;
 
   Container(coral::PipelinedModelRunner *runner_,
             tflite::Interpreter *interpreter_, bool use_multiple_edgetpu_,
-            GstElement *pipeline_,
-            std::unordered_map<int, std::string> labels_) {
+            GstElement *pipeline_, std::unordered_map<int, std::string> labels_,
+            bool visualize_inference_) {
     runner = runner_;
     interpreter = interpreter_;
     use_multiple_edgetpu = use_multiple_edgetpu_;
     pipeline = pipeline_;
     labels = labels_;
+    visualize_inference = visualize_inference_;
   }
+};
+
+// Struct for inference results
+struct Result {
+  int max_index;
+  float max_score;
+  std::string label;
 };
 
 // Struct for model pipeline runner and gloop
@@ -172,49 +181,55 @@ std::unique_ptr<tflite::Interpreter> SetUpIntepreter(
   return interpreter;
 }
 
-// Function dequantizes output tensor and prints out inference results
-// Inputs: uint8_t*, and TfLiteTensor*, unordered_map*, bool, GstElement
-void PrintInferenceResults(
-    const Container *container, const uint8_t *data,
-    const std::chrono::time_point<std::chrono::system_clock> start) {
+// Function dequantizes output tensor and returns one Result
+// Inputs: Container* and uint8_t*
+// Output: Result
+Result GetInferenceResults(const Container *container, const uint8_t *data) {
   const int output_index = container->interpreter->outputs()[0];
   const TfLiteTensor *out_tensor =
       CHECK_NOTNULL(container->interpreter->tensor(output_index));
   std::vector<float> inference_result(out_tensor->bytes);
-  static int frame_count = 0;
   // Dequantize output tensor
   for (size_t i = 0; i < inference_result.size(); i++) {
     inference_result[i] =
         (data[i] - out_tensor->params.zero_point) * out_tensor->params.scale;
   }
-  // Find and print out max score of each frame inference result
+  // Find max score, index, and label of each frame inference result
   const auto max_element =
       std::max_element(inference_result.begin(), inference_result.end());
   const int max_index = std::distance(inference_result.begin(), max_element);
   const float max_score = inference_result[max_index];
-  std::chrono::duration<double, std::milli> elapsed_time =
-      std::chrono::system_clock::now() - start;
   const std::string label = container->labels.at(max_index);
-  const std::string type = container->use_multiple_edgetpu
-                               ? "Pipeline Runner"
-                               : "tflite::Interpreter";
-  GstElement *overlaysink =
-      gst_bin_get_by_name(GST_BIN(container->pipeline), "overlaysink");
-  std::string svg = absl::Substitute(
-      "<svg baseProfile='full' height='1000' version='1.1' width='1000' "
-      "xmlns='http://www.w3.org/2000/svg' "
-      "xmlns:ev='http://www.w3.org/2001/xml-events' "
-      "xmlns:xlink='http://www.w3.org/1999/xlink'><defs /><text fill='white' "
-      "font-size='30' x='11' y='30'>Label: $0</text><text fill='white' "
-      "font-size='30' x='11' y='60'>Score: $1</text><text fill='white' "
-      "font-size='30' x='11' y='90'>Type: $2</text><text fill='white' "
-      "font-size='30' x='11' y='120'>Time: $3</text></svg>",
-      label, max_score, type, elapsed_time.count());
-  g_object_set(G_OBJECT(overlaysink), "svg", svg.c_str(), NULL);
+  Result final_result = {max_index, max_score, label};
+  return final_result;
+}
+
+// Function dequantizes output tensor and prints out inference results
+// Inputs: Container*, Result, double, string
+void PrintInferenceResults(const Container *container, const Result result,
+                           const double time, const std::string type) {
+  static int frame_count = 0;
+  if (container->visualize_inference) {
+    GstElement *overlaysink =
+        gst_bin_get_by_name(GST_BIN(container->pipeline), "overlaysink");
+    std::string svg = absl::Substitute(
+        "<svg baseProfile='full' height='1000' version='1.1' width='1000' "
+        "xmlns='http://www.w3.org/2000/svg' "
+        "xmlns:ev='http://www.w3.org/2001/xml-events' "
+        "xmlns:xlink='http://www.w3.org/1999/xlink'><defs /><text fill='white' "
+        "font-size='30' x='11' y='30'>Label: $0</text><text fill='white' "
+        "font-size='30' x='11' y='60'>Score: $1</text><text fill='white' "
+        "font-size='30' x='11' y='90'>Type: $2</text><text fill='white' "
+        "font-size='30' x='11' y='120'>Time: $3 ms</text></svg>",
+        result.label, result.max_score, type, time);
+    g_object_set(G_OBJECT(overlaysink), "svg", svg.c_str(), NULL);
+  }
   std::cout << "Frame " << frame_count++ << std::endl;
-  std::cout << "-----------------------------" << std::endl;
-  std::cout << "Label: " << label << std::endl;
-  std::cout << "Max Score: " << max_score << std::endl;
+  std::cout << "----------------------------------" << std::endl;
+  std::cout << "Label: " << result.label << std::endl;
+  std::cout << "Max Score: " << result.max_score << std::endl;
+  std::cout << type << ": " << time << " ms" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
 }
 
 // Function called on every new available sample, reads each frame data, and
@@ -262,14 +277,13 @@ GstFlowReturn OnNewSample(GstElement *sink, Container *container) {
             CHECK_NOTNULL(container->interpreter->tensor(output_index));
         const uint8_t *out_tensor_data =
             tflite::GetTensorData<uint8_t>(tensor_data);
-        PrintInferenceResults(container, out_tensor_data, start);
+        Result result = GetInferenceResults(container, out_tensor_data);
         std::chrono::duration<double, std::milli> elapsed_time =
             std::chrono::system_clock::now() - start;
+        PrintInferenceResults(container, result, elapsed_time.count(),
+                              "tflite::Interpreter");
         container->interpreter_total_time_ms += elapsed_time.count();
         container->interpreter_inference_count++;
-        std::cout << "Interpreter: " << elapsed_time.count() << " ms"
-                  << std::endl;
-        std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
       }
     } else {
       std::cout << "Error: Couldn't get buffer info" << std::endl;
@@ -293,12 +307,12 @@ void ResultConsumer(Container *runner_container) {
     // Retrieve output tensor and output tensor data
     coral::PipelineTensor out_tensor = output_tensors[0];
     const uint8_t *tensor_data = static_cast<uint8_t *>(out_tensor.data.data);
-    PrintInferenceResults(runner_container, tensor_data, start);
+    Result result = GetInferenceResults(runner_container, tensor_data);
     std::chrono::duration<double, std::milli> elapsed_time =
         std::chrono::system_clock::now() - start;
     start = std::chrono::system_clock::now();
-    std::cout << "Pipeline: " << elapsed_time.count() << " ms" << std::endl;
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    PrintInferenceResults(runner_container, result, elapsed_time.count(),
+                          "Pipeline Runner");
     coral::FreeTensors(output_tensors,
                        runner_container->runner->GetOutputTensorAllocator());
     output_tensors.clear();
@@ -394,9 +408,6 @@ gboolean OnBusMessage(GstBus *bus, GstMessage *msg, gpointer data) {
       gst_message_parse_warning(msg, &error, nullptr);
       g_printerr("Warning: %s\n", error->message);
       g_error_free(error);
-      // container->runner->Push({});
-      // container->loop_finished = true;
-      // g_main_loop_quit(loop);
       break;
     }
     default:
@@ -502,7 +513,7 @@ int main(int argc, char *argv[]) {
 
   auto *sink = gst_bin_get_by_name(GST_BIN(pipeline), "appsink");
   Container runner_container(runner.get(), interpreter.get(),
-                             use_multiple_edgetpu, pipeline, labels);
+                             use_multiple_edgetpu, pipeline, labels, visualize);
   g_signal_connect(sink, "new-sample", reinterpret_cast<GCallback>(OnNewSample),
                    &runner_container);
 
